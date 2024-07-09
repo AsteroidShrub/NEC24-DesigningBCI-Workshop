70830a11e6fa30b383632d7e23999a84deb6388e
Solfervin <115954381+Solfervin@users.noreply.github.com>
2024-07-09 09:28:41 +0200

    UP: skellys for nodes

--- 7- Handson/nodes/bands.py
+++ 7- Handson/nodes/bands.py
@@ -5,41 +5,22 @@ class BandExtraction(Node):
 
     def __init__(self, bands:str):
         ...
-        # self._reref_electrode = electrode
-
-    """Rereference the incoming EEG signal using the specified electrode
-
-    Attributes:
-        i (Port): EEG signal, expects DataFrame.
-        o (Port): Rereferenced EEG signal, provides DataFrame
-    """
 
     def update(self):
         if self.i.ready():
-            self.o.meta = self.i.meta
-            self.o.data = self.i.data
-            # self.o.data = self.o.data.subtract(self.o.data[self._reref_electrode], axis=0)
-            # self.o.data.drop(self._reref_electrode, axis=1, inplace=True)
-            # print(self.o.data)
+            # self.o.meta = self.i.meta
+            # self.o.data = self.i.data
+            ...
 
 
 class Relax(Node):
 
     def __init__(self):
         ...
-        # self._reref_electrode = electrode
-
-    """Rereference the incoming EEG signal using the specified electrode
-
-    Attributes:
-        i (Port): EEG signal, expects DataFrame.
-        o (Port): Rereferenced EEG signal, provides DataFrame
-    """
 
     def update(self):
         if self.i.ready():
-            self.o.meta = self.i.meta
-            self.o.data = self.i.data
-            # self.o.data = self.o.data.subtract(self.o.data[self._reref_electrode], axis=0)
-            # self.o.data.drop(self._reref_electrode, axis=1, inplace=True)
-            # print(self.o.data)
+            # self.o.meta = self.i.meta
+            # self.o.data = self.i.data
+            ...
+            
--- 7- Handson/nodes/electrodes.py
+++ 7- Handson/nodes/electrodes.py
@@ -4,15 +4,10 @@ from timeflux.core.node import Node
 class ElectrodeDeletion(Node):
 
     def __init__(self, electrodes:str):
-        self._reref_electrode = electrodes
+        self._electrodes = electrodes
 
-    """Rereference the incoming EEG signal using the specified electrode
-
-    Attributes:
-        i (Port): EEG signal, expects DataFrame.
-        o (Port): Rereferenced EEG signal, provides DataFrame
-    """
     def update(self):
         if self.i.ready():
-            self.o.meta = self.i.meta
-            self.o.data = self.i.data
+            # self.o.meta = self.i.meta
+            # self.o.data = self.i.data
+            ...
